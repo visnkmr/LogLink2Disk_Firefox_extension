7a6eade72b7e73360c94ab029c4ebb955882c1a7
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-04-29 21:48:11 +0530

    save_to_datefolder

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.tabs.query({currentWindow: true}, function(tabs) {
     
       fetch("http://127.0.0.1:8080", {
           method: 'PUT',
-          body: JSON.stringify({url: tab.url, title: tab.title, folder:"firefox"}),
+          body: JSON.stringify({url: tab.url, title: tab.title, folder:getTodayDateTimeString()}),
           headers: {
           'Content-type': 'application/json; charset=UTF-8'
           }
@@ -32,6 +32,25 @@ chrome.tabs.query({currentWindow: true}, function(tabs) {
   }
 });
 });
+function getTodayDateTimeString() {
+  // Create a new Date object with the current date and time
+  let today = new Date();
+  // Get the year, month, day, hour, minute and second from the Date object
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1; // Months are zero-based
+  let day = today.getDate();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  // Pad the month, day, hour, minute and second with leading zeros if needed
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+  // Return the date and time as a string in the format yyyy-mm-dd_hh-mm-ss
+  return year + "-" + month + "-" + day + "_" + hour + "-" + minute + "-" + second;
+  }
 
 //   function (tab) {
 //     console.log(tab)
