6cc41dceb455673f5e54b717048185151478b61c
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-04-30 19:23:46 +0530

    added Webpack
    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './background.ts', // the file with import statement
+  output: {
+    filename: 'bundle.js', // the output file that can be loaded by the browser
+    // path: path.resolve(__dirname, 'dist'), // the output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // match TypeScript files
+        use: 'ts-loader', // use ts-loader to transpile them
+        exclude: /node_modules/, // exclude node_modules directory
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // resolve both TypeScript and JavaScript extensions
+  },
+};
